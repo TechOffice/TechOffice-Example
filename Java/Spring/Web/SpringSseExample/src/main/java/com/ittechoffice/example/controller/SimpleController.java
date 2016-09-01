6cebf43dab04841122100defa8eff19cd9d9af59
Cheng Pan Wai <imben1109@Cheng-Pan-Wais-MacBook-Pro.local>
2016-09-02 01:42:16 +0800

    update

--- Java/Spring/Web/SpringSseExample/src/main/java/com/ittechoffice/example/controller/SimpleController.java
+++ Java/Spring/Web/SpringSseExample/src/main/java/com/ittechoffice/example/controller/SimpleController.java
@@ -1,34 +1,38 @@
-package com.ittechoffice.example.springmvc.controller;
+package com.ittechoffice.example.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.ittechoffice.example.sse.SseEmitterManager;
 
 @Controller
 public class SimpleController {
-	
-    private ResponseBodyEmitter emitter;
+		
+	@Autowired
+    private SseEmitterManager sseEmitterManager;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String hello(ModelMap model){
 		return "home";		
 	}
 	
-	@RequestMapping("/emit")
-	public ResponseBodyEmitter  handle() {
-	    return emitter;
+	@RequestMapping(value ="/emit")
+	public SseEmitter  handle() {
+	    return sseEmitterManager.createEmitter();
 	}
 
-	@RequestMapping("/send")
+	@RequestMapping(value="/send", method = RequestMethod.GET)
 	@ResponseBody
 	public String send(@RequestParam("message") String message) throws IOException{
-		emitter.send(message);
+		sseEmitterManager.send(message);
 		return message;
 	}
 	
