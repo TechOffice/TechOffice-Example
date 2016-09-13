dc0365f72959aadd16641a57655672cfb4e6412d
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-09-13 16:21:55 +0800

    update

---
+++ 030-Javascript/910-Dev/SimpleDiffViewer/src/simpleViewDiffArrConvertor.js
@@ -0,0 +1,43 @@
+function simpleViewDiffArrConvertor(diff){
+	var diffArr = [];
+	for (var i=3; i<diff.length; i++){
+		var line = diff[i];
+		if (line.startsWith(" ")){
+			var lineContent = line.substring(1);
+			diffArr.push(lineContent);
+		}
+		if (line.startsWith("-")){
+			var lineContent = line.substring(1);
+			var map;
+			if (diffArr[diffArr.length - 1] instanceof Map){
+				map = diffArr[diffArr.length - 1];
+			}else{
+				map = new Map();
+				diffArr.push(map);
+			}
+			var leftArr = map.get("left");
+			if (!leftArr){
+				leftArr = [];
+				map.set("left", leftArr);
+			}
+			leftArr.push(lineContent);
+		}
+		if (line.startsWith("+")){
+			var lineContent = line.substring(1);
+			var map;
+			if (diffArr[diffArr.length - 1] instanceof Map){
+				map = diffArr[diffArr.length - 1];
+			}else{
+				map = new Map();
+				diffArr.push(map);
+			}
+			var rightArr = map.get("right");
+			if (!rightArr){
+				rightArr = [];
+				map.set("right", rightArr);
+			}
+			rightArr.push(lineContent);
+		}
+	}
+	return diffArr;
+}
