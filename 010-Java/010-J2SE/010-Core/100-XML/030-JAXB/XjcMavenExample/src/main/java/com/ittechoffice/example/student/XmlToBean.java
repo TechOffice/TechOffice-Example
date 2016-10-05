4ce214802079f3a954226cc1f031d80aab87cf71
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-10-05 15:16:19 +0800

    update

--- 010-Java/010-J2SE/010-Core/100-XML/030-JAXB/XjcMavenExample/src/main/java/com/ittechoffice/example/student/XmlToBean.java
+++ 010-Java/010-J2SE/010-Core/100-XML/030-JAXB/XjcMavenExample/src/main/java/com/ittechoffice/example/student/XmlToBean.java
@@ -3,6 +3,7 @@ package com.ittechoffice.example.student;
 import java.io.InputStream;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
@@ -10,18 +11,13 @@ import javax.xml.transform.stream.StreamSource;
 import com.ittechoffice.example.student.model.Student;
 
 public class XmlToBean {
-	public static void main(String[] args) throws JAXBException{
+	public static void main(String[] args) throws JAXBException {
 		InputStream stream = XmlToBean.class.getClassLoader().getResourceAsStream("student.xml");
-		
 		JAXBContext jaxbContext = JAXBContext.newInstance(Student.class);
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		
-//		 StreamSource xml = new StreamSource("src/resources/student.xml");
-//		         Unmarshaller unmarshaller = jc.createUnmarshaller();
-//		         JAXBElement<Customer> je1 = unmarshaller.unmarshal(xml, Customer.class);
-//		         Customer customer = je1.getValue();
-
-
-
+		StreamSource streamSource = new StreamSource(stream);
+		JAXBElement<Student> studentJaxbElement = jaxbUnmarshaller.unmarshal(streamSource, Student.class);
+		Student student = studentJaxbElement.getValue();
+		System.out.println(student.getName());
 	}
 }
