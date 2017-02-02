a87d43872a8b90e84398d1063de5abb530fb3254
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-02-02 18:40:15 +0800

    update

---
+++ 080-C++/030-Others/010-Image/010-OpenCV/OpenCvExample/README.md
@@ -0,0 +1,7 @@
+property -> C/C++ Build -> Settings -> Tool Settings -> Cygwin C++ Linker -> Libraries
+Add two library 
+* opencv_highgui
+* opencv_core
+
+Build and Run 
+The Opencv Lib could be used.
---
+++ 080-C++/030-Others/010-Image/010-OpenCV/OpenCvExample/src/OpenCvExample.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : OpenCvExample.cpp
+// Author      : Ben
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+int main() {
+
+	cv::Mat m ;
+
+	return 0;
+}
